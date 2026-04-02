1ab2ebaff9a9a5e75883228723b14f183bafbe87
abdulazizdroid <abdulazizbot80@gmail.com>
2026-04-02 21:14:14 +0500

    Abdulaziz

--- src/main/java/students/cswithgithub/Controller/CsController.java
+++ src/main/java/students/cswithgithub/Controller/CsController.java
@@ -1,4 +1,21 @@
 package students.cswithgithub.Controller;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.connector.Response;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import students.cswithgithub.CSDAO.DAO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/Cs")
+
 public class CsController {
+
+    @PutMapping("/update")
+    public Response update (@RequestBody DAO dao){
+        return null;
+    }
 }
