774ec6d39bbea110d71286e9fd73c6cfe251fb7d
user <abdubayevsalohiddin@gmail.com>
2026-04-02 20:49:51 +0500

    !

--- src/main/java/students/cswithgithub/Controller/CsController.java
+++ src/main/java/students/cswithgithub/Controller/CsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import students.cswithgithub.DTO.CsDTO;
 
 @RestController
-    @RequestMapping("/dori")
+    @RequestMapping("/Cs")
 
     public class CsController {
 
