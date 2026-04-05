f5b744dfcf03e6920a71144339dc01222049e4c7
dilshodbekyusufjanov5258-oss <dilshodbekyusufjanov5258@gmail.com>
2026-04-05 20:08:09 +0500

    CsRepo ga Jpa qo'shildi!

--- src/main/java/students/cswithgithub/Repository/CsRepository.java
+++ src/main/java/students/cswithgithub/Repository/CsRepository.java
@@ -1,6 +1,7 @@
 package students.cswithgithub.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import students.cswithgithub.DAO.CsDAO;
 
-public interface CsRepository extends JpaRepository {
+public interface CsRepository extends JpaRepository<CsDAO, Integer> {
 }
