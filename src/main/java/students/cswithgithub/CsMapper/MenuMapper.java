6782fae763b0202f6ed2975e2ccd3ea509f9c488
user <abdubayevsalohiddin@gmail.com>
2026-04-05 20:33:47 +0500

    mapper

---
+++ src/main/java/students/cswithgithub/CsMapper/MenuMapper.java
@@ -0,0 +1,20 @@
+package students.cswithgithub.CsMapper;
+
+import org.mapstruct.Mapping;
+import students.cswithgithub.DAO.CsDAO;
+import students.cswithgithub.DTO.CsDTO;
+
+import java.util.List;
+
+
+    @org.mapstruct.Mapper(componentModel = "spring")
+    public interface MenuMapper {
+
+        @Mapping(target = "id", ignore = true)
+        CsDAO toDao(CsDTO csDTO);
+
+        CsDTO toDto(CsDAO csDAO);
+
+        List<CsDTO> toDTOList(List<CsDAO>menuDAOListO);
+    }
+
